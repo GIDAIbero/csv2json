2edab39d46a5401ca82e40baa8b5d414738b9827
Alejandro Paredes <alejandroparedes@gmail.com>
2013-03-12 16:29:05 +0100

    More clean up, Correct print out
    Print out to stdout.
    Fix printing for subtitles (specially last one)
    clean up for validacion

--- Insertar.c
+++ Insertar.c
@@ -1,14 +1,14 @@
 #include "Insertar.h"
 
 void Pasar_cadenas(char matriz[100][100], int renglones, int error, ptrtema *Inicio, char tema[100], char titulo[100], char subtitulo[100], ptrsubtema *ultimo_subtema, int *posicion_comas) {
-	int i, j, k=0, l, m;
+	int i, j, k=0, l, m, quo = 0;
 
 	*posicion_comas=0;
 	if(error==0) {
 		for(i=0; i<renglones-1; i++) {
 			if(matriz[i][0]!=',') {
 				k=0;
-				for(j=0; matriz[i][j]!=','; j++) {
+				for(j=0; matriz[i][j]!=',' ; j++) {
 					tema[k]=matriz[i][j];
 					k++;
 				}
@@ -18,28 +18,38 @@ void Pasar_cadenas(char matriz[100][100], int renglones, int error, ptrtema *Ini
 			}
 			if(matriz[i][0]==',') {
 				k=0;
-				for(j=1; matriz[i][j]!='\n'; j++) {
-					if(matriz[i][j]==',') {
+				quo = 0;
+				for(j=1; matriz[i][j]!='\n' && matriz[i][j]!='\0'; j++) {
+					if (matriz[i][j] == '"') {
+						if (quo == 0) {
+							quo = 1;
+						} else {
+							quo = 0;
+						}
+					}
+					if(matriz[i][j]==',' && quo == 0) {
 						*posicion_comas=j;
 					}
 				}
-				if(posicion_comas!=0) {
+				if(*posicion_comas!=0) {
 					for(l=1; l<*posicion_comas; l++) {
-						titulo[k]=matriz[i][l];
-						k++;
+						if (matriz[i][l] != '"') {
+							titulo[k]=matriz[i][l];
+							k++;
+						}
 					}	  
 					titulo[k]='\0';
 					k=0;
-					for(m=*posicion_comas+1; matriz[i][m]!='\n'; m++) {
+					for(m=*posicion_comas+1; matriz[i][m]!='\n' && matriz[i][m]!='\0'; m++) {
 						subtitulo[k]=matriz[i][m];
 						k++;
 					}
-					subtitulo[k]='\0';
+					subtitulo[k-1]='\0';
 					Insertar_subtema(*Inicio, titulo, subtitulo, ultimo_subtema, *posicion_comas);
 				}
 				if(*posicion_comas==0) {
 					k=0;
-					for(l=1; matriz[i][l]!='\n'; l++) {
+					for(l=1; matriz[i][l]!='\n' && matriz[i][l]!='\0'; l++) {
 						titulo[k]=matriz[i][l];
 						k++;
 					}	  
@@ -170,84 +180,115 @@ void Muestra(ptrtema Inicio, char Nombre_salida[100], int posicion_comas) {
 		printf("La lista esta vacia\n");
 	} else {
 		fprintf(Archivo, "\133\n");
+		printf("\133\n");
 		if((Inicio->Next_tema)==NULL) {
 			fprintf(Archivo, "   \133");
 			fprintf(Archivo, "\"%s\"\n", Inicio->tema);
 			fprintf(Archivo, "],\n");
+			printf("   \133");
+			printf("\"%s\"\n", Inicio->tema);
+			printf("],\n");
 		} else {
 			fprintf(Archivo, "   \133");
+			printf("   \133");
 			while(Mostrar->Next_tema!=NULL) {
 				fprintf(Archivo, "\"%s\",", Mostrar->tema);
+				printf("\"%s\",", Mostrar->tema);
 				Mostrar=Mostrar->Next_tema;
 			}
 			fprintf(Archivo, "\"%s\"", Mostrar->tema);
 			fprintf(Archivo, "],\n");
+			printf("\"%s\"", Mostrar->tema);
+			printf("],\n");
 		}
 		Mostrar=Inicio;
 		fprintf(Archivo, "   \133\n");
+		printf("   \133\n");
 		while (Mostrar->Next_tema!=NULL) {
 			fprintf(Archivo, "      \133");
+			printf("      \133");
 			ultimo=Mostrar->Next_subtema;
 			while(ultimo->Next_subtema!=NULL) {
 				if((*ultimo->titulo)!='\n' && (*ultimo->titulo)!=' ' && (*ultimo->titulo)!='\0') {
 					fprintf(Archivo, "\"%s\",", ultimo->titulo);
+					printf("\"%s\",", ultimo->titulo);
 				}
 				ultimo=ultimo->Next_subtema;
 			}
 			if((*ultimo->titulo)!='\n' && (*ultimo->titulo)!=' ' && (*ultimo->titulo)!='\0') {
 				fprintf(Archivo, "\"%s\"", ultimo->titulo);
+				printf("\"%s\"", ultimo->titulo);
 			}
 			Mostrar=Mostrar->Next_tema;
 			fprintf(Archivo, "]");
+			printf("]");
 			if(Mostrar->Next_subtema!=NULL){
 				fprintf(Archivo, ",\n");
+				printf(",\n");
 			}
 		}
 		fprintf(Archivo, "      \133");
+		printf("      \133");
 		ultimo=Mostrar->Next_subtema;
 		while(ultimo->Next_subtema!=NULL) {
 			if((*ultimo->titulo)!='\n' && (*ultimo->titulo)!=' ' && (*ultimo->titulo)!='\0') {
 				fprintf(Archivo, "\"%s\",", ultimo->titulo);
+				printf("\"%s\",", ultimo->titulo);
 			}
 			ultimo=ultimo->Next_subtema;
 		}
 		fprintf(Archivo, "\"%s\"", ultimo->titulo);
 		fprintf(Archivo, "]\n");
 		fprintf(Archivo, "   ]\n");
-      	if(posicion_comas!=0) {
-			fprintf(Archivo, "   \133\n");
+      	printf("\"%s\"", ultimo->titulo);
+		printf("]\n");
+		printf("   ]\n");
+      	if (posicion_comas!=0) {
+			fprintf(Archivo, "   ,\133\n");
+			printf("   ,\133\n");
 			Mostrar=Inicio;
 			while (Mostrar->Next_tema!=NULL) {
 				fprintf(Archivo, "      \133");
+				printf("      \133");
 				ultimo=Mostrar->Next_subtema;
 				while(ultimo->Next_subtema!=NULL) {
 					if((*ultimo->subtitulo)!='\n' && (*ultimo->subtitulo)!=' ' && (*ultimo->subtitulo)!='\0') {
 						fprintf(Archivo, "\"%s\",", ultimo->subtitulo);
+						printf("\"%s\",", ultimo->subtitulo);
 					}
 					ultimo=ultimo->Next_subtema;
 				}
 				if((*ultimo->subtitulo)!='\n' && (*ultimo->subtitulo)!=' ' && (*ultimo->subtitulo)!='\0') {
 					fprintf(Archivo, "\"%s\"", ultimo->subtitulo);
+					printf("\"%s\"", ultimo->subtitulo);
 				}
 				fprintf(Archivo, "]");
+				printf("]");
 				Mostrar=Mostrar->Next_tema;
 				fprintf(Archivo, ",\n");
+				printf(",\n");
 			}
 			ultimo=Mostrar->Next_subtema;
+			fprintf(Archivo, "      \133");
+			printf("      \133");
 			while(ultimo->Next_subtema!=NULL) {
-				fprintf(Archivo, "      \133");
 				if((*ultimo->subtitulo)!='\n' && (*ultimo->subtitulo)!=' ' && (*ultimo->subtitulo)!='\0') {
 					fprintf(Archivo, "\"%s\",", ultimo->subtitulo);
+					printf("\"%s\",", ultimo->subtitulo);
 				}
 				ultimo=ultimo->Next_subtema;
 			}
 			if((*ultimo->subtitulo)!='\n' && (*ultimo->subtitulo)!=' ' && (*ultimo->subtitulo)!='\0') {
 				fprintf(Archivo, "\"%s\"", ultimo->subtitulo);
+				printf("\"%s\"", ultimo->subtitulo);
 			}
 			fprintf(Archivo, "]\n");
 			fprintf(Archivo, "   ]\n");
+			printf("]\n");
+			printf("   ]\n");
 		}
 		fprintf(Archivo, "]\n");
+		printf("]\n");
 	}
 	fclose(Archivo);
 }
--- validacion.c
+++ validacion.c
@@ -1,11 +1,9 @@
 #include "validacion.h"
 
-void Despliega_menu (char Nombre_entrada[100],char Nombre_salida[100])
-{
+void Despliega_menu (char Nombre_entrada[100],char Nombre_salida[100]) {
   char ow[10];
   int i=0;
-  do
-  {
+  do {
     system("clear");
     printf("*******MENU********\n\n");
     printf("Dame el nombre del archivo de entrada (.csv): ");
@@ -14,91 +12,67 @@ void Despliega_menu (char Nombre_entrada[100],char Nombre_salida[100])
     scanf("%s", Nombre_salida);
     printf("¿Deseas omitir el mensaje de sobreescritura si el archivo de salida ya exitse?: ");
     scanf("%s", ow);
-    if(strcasecmp(ow, "no")==0)
-    {
-     i=3;
-	  //printf("entra a no\n");
-   }
-   if (strcasecmp(ow, "si")==0)
-   {
-     i=1;
-	  //printf("entra a si \n");
-   }
-   if(strcasecmp(ow, "renombrar")==0)
-     i=2;
-      //printf("sale de si\n");
-      //printf("%d\n", i);
-   if(i==0)
-   {
-     printf("ERROR, opción no válida.\n");
-     printf("Opciones válidas: si, no, renombrar\n");
-   }
- }
- while (i!=1 && i!=3);
- Menu_Entrada_Salida(Nombre_entrada, Nombre_salida);
- if(i==3)
-  if(Archivo_NULL(Nombre_salida)==0)
-  {
-   printf("Ese archivo ya existe.\n");
-   exit(0);
- }
-  //printf("sale del while\n");
+    if (strcasecmp(ow, "no")==0) {
+      i=3;
+    }
+    if (strcasecmp(ow, "si")==0) {
+      i=1;
+    }
+    if (strcasecmp(ow, "renombrar")==0) {
+      i=2;
+    }
+    if (i==0) {
+      printf("ERROR, opción no válida.\n");
+      printf("Opciones válidas: si, no, renombrar\n");
+    }
+  } while (i!=1 && i!=3);
+  Menu_Entrada_Salida(Nombre_entrada, Nombre_salida);
+  if (i==3){
+    if (Archivo_NULL(Nombre_salida)==0) {
+      printf("Ese archivo ya existe.\n");
+      exit(0);
+    }
+  }
 }
 
-int validacion(int argc, char *argv[], char Nombre_entrada[100], char Nombre_salida[100])
-{
+int validacion(int argc, char *argv[], char Nombre_entrada[100], char Nombre_salida[100]) {
   int i=0;
   if (argc<2||argc>6)
     Despliega_error();
-  else
-  {
-    if (argc==2)
-    {
-     if (strcmp(argv[1], "-menu")==0)
-     {
-       Despliega_menu(Nombre_entrada, Nombre_salida);
-       i++;
-     }
-     else
-       Despliega_error();
-   }
-   if (argc==3 || argc==4)
-     Despliega_error();
-   if(argc==5)
-   {
-     if (strcmp(argv[1],"-i")==0)
-     {
-       if (strcmp(argv[3], "-o")==0)
-       {
+  else {
+    if (argc==2) {
+      if (strcmp(argv[1], "-menu")==0) {
+        Despliega_menu(Nombre_entrada, Nombre_salida);
         i++;
-        Entrada_Salida(argv, Nombre_entrada, Nombre_salida);
-      }
-    }
-    else
-     Despliega_error();
- }
- if(argc==6)
- {
-   if (strcmp(argv[1],"-i")==0)
-   {
-     if (strcmp(argv[3], "-o")==0)
-     {
-      if(strcmp(argv[5], "-ow")==0)
-      {
-        i++;
-        Entrada_Salida(argv, Nombre_entrada, Nombre_salida);
-      }
-      else
-        Despliega_error();
-    }
-    else
+      } else
       Despliega_error();
+    }
+    if (argc==3 || argc==4)
+      Despliega_error();
+    if (argc==5) {
+      if (strcmp(argv[1],"-i")==0) {
+        if (strcmp(argv[3], "-o")==0) {
+          i++;
+          Entrada_Salida(argv, Nombre_entrada, Nombre_salida);
+        }
+      } else
+      Despliega_error();
+    }
+    if (argc==6) {
+      if (strcmp(argv[1],"-i")==0) {
+        if (strcmp(argv[3], "-o")==0) {
+          if(strcmp(argv[5], "-ow")==0) {
+            i++;
+            Entrada_Salida(argv, Nombre_entrada, Nombre_salida);
+          } else
+          Despliega_error();
+        } else
+        Despliega_error();
+      } else
+      Despliega_error();
+    }
   }
-  else
-   Despliega_error();
-}
-}
-return i;
+  return i;
 }
 
 void Despliega_error()
@@ -109,49 +83,50 @@ void Despliega_error()
   printf("                            $CSV2JSON -i entrada -o salida -ow\n\n");
 }
 
-void Entrada_Salida(char *argv[], char *Entrada, char *Salida)
-{
+void Entrada_Salida(char *argv[], char *Entrada, char *Salida) {
   int a;
   strcpy(Entrada, argv[2]);
   strcpy(Salida, argv[4]);
-  //printf("%s & %s\n", Entrada, Salida);
-  //Entrada
-  for(a=0; Entrada[a]!='.' && Entrada[a]!='\0'; a++)
-  {}
-if(Entrada[a]=='.')
-  if(strcmp(&Entrada[a], ".csv")!=0)
-    strcpy(&Entrada[a],".csv");
+  
+  for(a=0; Entrada[a]!='.' && Entrada[a]!='\0'; a++) {
+  }
+  if(Entrada[a]=='.') {
+    if(strcmp(&Entrada[a], ".csv")!=0)
+      strcpy(&Entrada[a],".csv");
+  }
   if(Entrada[a]!='.')
     strcat(Entrada,".csv");
-  //Salida
-  for(a=0; Salida[a]!='.' && Salida[a]!='\0'; a++)
-  {}
-if(Salida[a]=='.')
-  if(strcmp(&Salida[a], ".json")!=0)
-    strcpy(&Salida[a],".json");
+
+  for(a=0; Salida[a]!='.' && Salida[a]!='\0'; a++) {
+  }
+  if(Salida[a]=='.') {
+    if(strcmp(&Salida[a], ".json")!=0)
+      strcpy(&Salida[a],".json");
+  }
   if(Salida[a]!='.')
     strcat(Salida,".json");
-  //printf("%s & %s\n", Entrada, Salida);
 }
 
 void Menu_Entrada_Salida(char *Entrada, char *Salida) {
   int a;
-  for(a=0; Entrada[a]!='.' && Entrada[a]!='\0'; a++) { }
-    if(Entrada[a]=='.'){
-      if(strcmp(&Entrada[a], ".csv")!=0) {
-        strcpy(&Entrada[a],".csv");
-      }
+  for(a=0; Entrada[a]!='.' && Entrada[a]!='\0'; a++) {
+  }
+  if(Entrada[a]=='.'){
+    if(strcmp(&Entrada[a], ".csv")!=0) {
+      strcpy(&Entrada[a],".csv");
     }
-    if(Entrada[a]!='.') {
-      strcat(Entrada,".csv"); 
+  }
+  if(Entrada[a]!='.') {
+    strcat(Entrada,".csv"); 
+  }
+  for(a=0; Salida[a]!='.' && Salida[a]!='\0'; a++) {
+  }
+  if(Salida[a]=='.') {
+    if(strcmp(&Salida[a], ".json")!=0) {
+      strcpy(&Salida[a],".json");
     }
-    for(a=0; Salida[a]!='.' && Salida[a]!='\0'; a++) {}
-      if(Salida[a]=='.') {
-        if(strcmp(&Salida[a], ".json")!=0) {
-          strcpy(&Salida[a],".json");
-        }
-      }
-      if(Salida[a]!='.') {
-        strcat(Salida,".json");
-      }
-    }
+  }
+  if(Salida[a]!='.') {
+    strcat(Salida,".json");
+  }
+}
